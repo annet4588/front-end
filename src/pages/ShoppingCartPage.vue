a43ea9e9501ef65b217a4cd21d1fb35ab8243399
BroAnna <annet4588@yahoo.co.uk>
2024-01-11 22:10:42 +0000

    first commit

---
+++ src/pages/ShoppingCartPage.vue
@@ -0,0 +1,28 @@
+
+<template>
+    <h1>Shopping Cart</h1>
+    <div v-if="cartItems.length > 0">
+        <ShoppingCartList :products="cartItems"/>
+        <button class="checkout-button">Proceed to Checkout</button>
+    </div>
+    <div v-if="cartItems.length === 0">You have no items in your cart</div> 
+</template>  
+
+<script>
+import ShoppingCartList from '@/components/ShoppingCartList.vue'
+import {cartItems} from '../temp-data'
+
+export default{
+    name: "ShoppingCartPage",
+    components: {
+        ShoppingCartList,
+    },
+    data(){
+        // eslint-disable-next-line no-unused-vars
+        const unusedVariable = cartItems; // This line will not trigger the 'no-unused-vars' warning
+        return{
+            cartItems,
+        }
+    }
+}
+</script>
